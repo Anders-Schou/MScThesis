b98b735320f16a755457095a279dba70534751c0
Anders-Schou <anders.emil.schou@gmail.com>
2024-03-01 15:38:57 +0100

    Added another test script

--- utils/utils.py
+++ utils/utils.py
@@ -19,3 +19,14 @@ def keep_points(arr: jnp.ndarray, fun: Callable) -> jnp.ndarray:
 def out_shape(fun, *args):
     print(f"{fun.__name__:<15}    {jax.eval_shape(fun, *args).shape}")
 
+
+def limits2vertices(xlim: Sequence, ylim: Sequence) -> list[tuple[list]]:
+    # Works for rectangles only. Returns pairs of end points for
+    # each of the four sides in counterclockwise order.
+    v = [
+         ([xlim[0], ylim[0]], [xlim[1], ylim[0]]), # Lower horizontal
+         ([xlim[1], ylim[0]], [xlim[1], ylim[1]]), # Right vertical
+         ([xlim[1], ylim[1]], [xlim[0], ylim[1]]), # Upper horizontal
+         ([xlim[0], ylim[1]], [xlim[0], ylim[0]])  # Left vertical
+    ]
+    return v
