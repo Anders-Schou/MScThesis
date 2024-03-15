68b5e28de5f79ac655083fa4e6f1ad2caf3a9ad4
Anders-Schou <anders.emil.schou@gmail.com>
2024-03-15 12:32:12 +0100

    Added diff functions

--- models/derivatives.py
+++ models/derivatives.py
@@ -1,7 +1,9 @@
 from collections.abc import Callable
 
+import numpy as np
 import jax
 import jax.numpy as jnp
+
 
 def hessian(model) -> Callable:
     return jax.hessian(model, argnums=1)
@@ -17,3 +19,45 @@ def biharmonic(model, axis1=1, axis2=2) -> Callable:
     lap = laplacian(model, axis1=axis1, axis2=axis2)
     lap2 = laplacian(lap, axis1=axis1, axis2=axis2)
     return lap2
+
+
+def diff_xx(model) -> Callable:
+    def xx(params, input):
+        return jax.vmap(jax.hessian(model, argnums=1), in_axes=(None, 0))(params, input)
+    return xx
+
+
+def diff_yy(model) -> Callable:
+    def yy(params, input):
+        return jax.vmap(hessian(model), in_axes=(None, 0))(params, input)[:, 1, 1]
+    return yy
+
+
+def diff_xy(model) -> Callable:
+    def xy(params, input):
+        return jax.vmap(hessian(model), in_axes=(None, 0))(params, input)[:, 0, 1]
+    return xy
+
+
+def finite_diff_xx(model, h=None) -> Callable:
+    if h is None:
+        h = np.cbrt(np.finfo(np.float32).eps)
+    
+    h2 = h**2
+    def fdmx2d(params, input):
+        dx = jnp.array([[h, 0]])
+        xx = (model(params, jnp.add(input, dx)) + model(params, jnp.subtract(input, dx)) - 2*model(params, input)) / h2
+        return xx
+    return fdmx2d
+
+
+def finite_diff_yy(model, h=None) -> Callable:
+    if h is None:
+        h = np.cbrt(np.finfo(np.float32).eps)
+        
+    h2 = h**2
+    def fdmy2d(params, input):
+        dy = jnp.array([[0, h]])
+        xx = (model(params, jnp.add(input, dy)) + model(params, jnp.subtract(input, dy)) - 2*model(params, input)) / h2
+        return xx
+    return fdmy2d
